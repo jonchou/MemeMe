8a9d0302ce5adc39f8162a51d0d383b4417c820a
jonchou <jonchou@ucdavis.edu>
2015-09-17 18:46:23 -0700

    Added ability to pick an image from album and have it be displayed after

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePickerView.contentMode = UIViewContentMode.ScaleAspectFit
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        //cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
-
+    
+    // Pick an image (from album?)
+    @IBAction func pickAnImage(sender: AnyObject) {
+    
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        self.presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
+    // Pick an image from the Album
+    @IBAction func pickAnImageFromAlbum (sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    // Pick an image from the camera
+    @IBAction func pickAnImageFromCamera (sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    // Once you pick an image, assign it to the UIImageView
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]){
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imagePickerView.image = image
+        } else {
+                print("oops")
+        }
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // Dismiss the view controller when user clicks cancel
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
